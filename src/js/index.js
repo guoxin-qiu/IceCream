bf2762abf2509a2be85b621c2a98583d16988737
Qiu <gqiu2@its.jnj.com>
2017-07-07 08:51:08 +0800

    添加模态窗口

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
 require(['./libs/require-config'], function () {
     require(['vue', 'ajax', 'header', 'footer'], function (Vue, ajax) {
+        Vue.component('modal',{
+            template: '#modal-template'
+        });
+
         new Vue({
             el: '#content',
             data: {
                 tabIndex: 1,
                 message: 'Welcome to enjoy bonbonniere vue simple sample.',
                 columns: ['Username', 'FullName', 'Email'],
-                users: []
+                users: [],
+                showModal: false
             },
             methods: {
                 getHref: function (id) {
