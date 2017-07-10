b69b0a54cba637231112a99e04ff882a17a89c43
Qiu <gqiu2@its.jnj.com>
2017-07-10 08:55:08 +0800

    从首页分离用户管理功能

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,10 @@
 require(['./libs/require-config'], function () {
-    require(['vue', 'ajax', 'page-template', 'modal'], function (Vue, ajax) {
+    require(['vue', 'page-template'], function (Vue) {
         new Vue({
             el: '#app',
             data: {
                 tabIndex: 1,
                 message: 'Welcome to enjoy bonbonniere vue simple sample.',
-                columns: ['Username', 'FullName', 'Email'],
-                users: [],
-                showModal: false,
-                curEditUser: {}
-            },
-            methods: {
-                openEdit: function (id) {
-                    debugger
-                    var _self = this;
-                    ajax.get('/User', {
-                        id: id
-                    }, function (response) {
-                        if (response.Success) {
-                            _self.curEditUser = response.User;
-                            _self.showModal = true;
-
-                        }
-                    });
-                },
-                saveUser: function () {
-                    alert(this.curEditUser.Username + ', ' + this.curEditUser.FullName + ', ' + this.curEditUser.Email);
-                },
-        
-            },
-            created: function () {
-                var _self = this;
-                ajax.get('/User', null, function (response) {
-                    if (response.Success) {
-                        _self.$data.users = response.Users;
-                    }
-                });
             }
         });
     });
---
+++ src/js/user.js
@@ -0,0 +1,40 @@
+require(['./libs/require-config'], function () {
+    require(['vue', 'ajax', 'page-template', 'modal'], function (Vue, ajax) {
+        new Vue({
+            el: '#app',
+            data: {
+                columns: ['Username', 'FullName', 'Email'],
+                users: [],
+                showModal: false,
+                curEditUser: {}
+            },
+            methods: {
+                openEdit: function (id) {
+                    debugger
+                    var _self = this;
+                    ajax.get('/User', {
+                        id: id
+                    }, function (response) {
+                        if (response.Success) {
+                            _self.curEditUser = response.User;
+                            _self.showModal = true;
+
+                        }
+                    });
+                },
+                saveUser: function () {
+                    alert(this.curEditUser.Username + ', ' + this.curEditUser.FullName + ', ' + this.curEditUser.Email);
+                },
+        
+            },
+            created: function () {
+                var _self = this;
+                ajax.get('/User', null, function (response) {
+                    if (response.Success) {
+                        _self.$data.users = response.Users;
+                    }
+                });
+            }
+        });
+    });
+});
